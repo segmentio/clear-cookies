9e6e1871f74d19a2cabd4772946c8dbb054b722e
Han Kim <han@segment.com>
2017-01-03 14:14:14 -0800

    stub out ie8 too

--- karma.conf.ci.js
+++ karma.conf.ci.js
@@ -39,11 +39,11 @@ var customLaunchers = {
   //   browserName: 'internet explorer',
   //   version: '7'
   // },
-  sl_ie_8: {
-    base: 'SauceLabs',
-    browserName: 'internet explorer',
-    version: '8'
-  },
+  // sl_ie_8: {
+  //   base: 'SauceLabs',
+  //   browserName: 'internet explorer',
+  //   version: '8'
+  // },
   sl_ie_9: {
     base: 'SauceLabs',
     browserName: 'internet explorer',
